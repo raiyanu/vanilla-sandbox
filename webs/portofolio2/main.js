6ed6a89c5f08161994359a37021dddc96ecbafa9
Raiyan Ahmed <ray38250968@gmail.com>
2022-12-25 09:57:25 +0000

    Add files via upload

---
+++ webs/portofolio2/main.js
@@ -0,0 +1,8 @@
+function show_menu() {
+  var menupopy = document.getElementById("menupop");
+  menupopy.classList.add("active");
+}
+function hide_menu() {
+  var menupopy = document.getElementById("menupop");
+  menupopy.classList.remove("active");
+}
