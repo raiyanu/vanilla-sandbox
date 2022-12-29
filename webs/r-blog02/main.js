86ec299700e8a07f7a6aa4ea0a213b0bbb5439f4
Raiyan Ahmed <ray38250968@gmail.com>
2022-12-29 15:57:32 +0000

    Add files via upload
    this is my another R-blog site but anime version this is will be keep going and updating always and forever 
    , don't worry dynamic features will be available soon as soon i learn javascript  firebase or supabase

---
+++ webs/r-blog02/main.js
@@ -0,0 +1,8 @@
+function show_menu() {
+  var menupopy = document.getElementById("menubar");
+  menupopy.classList.add("menuactive");
+}
+function hide_menu() {
+  var menupopy = document.getElementById("menubar");
+  menupopy.classList.remove("menuactive");
+}
