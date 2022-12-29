b83b6a41a940ea4f8eac7edd2b705c4f068503c1
Raiyan Ahmed <ray38250968@gmail.com>
2022-12-29 17:39:51 +0000

    Add files via upload

--- webs/r-blog02/main.js
+++ webs/r-blog02/main.js
@@ -6,3 +6,25 @@ function hide_menu() {
   var menupopy = document.getElementById("menubar");
   menupopy.classList.remove("menuactive");
 }
+
+ var mybutton = document.getElementById("mybutton");
+var audio = document.getElementById("mysong");
+ var my = document.getElementById("start");
+ audio. play();
+var count =1;
+var songicon = document.getElementById("songicon");
+var icon = document.getElementById("songicon");
+function playsong(){
+      if(count==0){
+            icon.classList.add("activated");
+            count = 1;
+            audio.play();
+            my.innerHTML = "⏸ Stop";
+      } else {
+            icon.classList.remove("activated");
+
+            count = 0;
+            audio.pause();
+            my.innerHTML = " ▶ Play";
+      }
+}
