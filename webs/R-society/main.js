7883ac0007e9a3dc5362208b0fbc0d4f88a73e99
Raiyan Ahmed <ray38250968@gmail.com>
2023-01-09 13:15:35 +0530

    Add files via upload

---
+++ webs/R-society/main.js
@@ -0,0 +1,44 @@
+var menu = document.getElementById("menu");
+menu.style.display = "flex";
+
+var ham = document.getElementById("ham");
+var closer = document.getElementById("closer");
+ham.style.display = "none";
+
+function show_player() {
+      var player = document.getElementById("player");
+      player.classList.toggle("musciplayer-active");
+      if (player.style.display === "block") {
+        player.style.display = "none";
+      } else{
+        player.style.display = "block";
+      }
+      var musicdot = document.getElementById("musicdot");
+      musicdot.classList.toggle('noopacity')
+}
+function show_extra(selection){
+  if(selection===0){
+    var setting = document.getElementById("setting");
+    if(setting.style.display === "flex"){
+      setting.style.display = "none";
+    } 
+    else {
+      setting.style.display = "flex";
+    }
+  }
+  if(selection===1){
+    if( menu.style.display === "flex"){
+      document.getElementById("main").style.width = "100%";
+      document.getElementById("main").style.border_bottom_left_radius = "1.5rem";
+      menu.style.display = "none";
+      closer.style.display = "none";
+      ham.style.display = "block";
+    } 
+    else {
+      document.getElementById("main").style.width = "inherit";
+      menu.style.display = "flex";
+      closer.style.display = "block";
+      ham.style.display = "none";
+    }
+  }
+}
---
+++ webs/R-society/task.md
@@ -0,0 +1,8 @@
+# task 
+
+- make svg close and open animation for menu bar btn 
+it should become hamburger and close symbol smoothly oh yeah 
+
+- create a good simple logo for r-society website 
+
+- design a chatting bar for website 
