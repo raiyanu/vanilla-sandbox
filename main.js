5db51579677acf4dbe1832e534e595dab6102f15
ray <raiyan.c.me@gmail.com>
2023-10-28 23:53:25 +0530

    added sign up with animation

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ function login() {
     Password = password.value;
     username.value = "";
     password.value = "";
-    if(!(User==''|| Password==''))alert(`hey mr/ms. ${User}, shhh your password is:${Password}`);
+    if (!(User == '' || Password == '')) alert(`hey mr/ms. ${User}, shhh your password is:${Password}`);
+}
+
+function switch_entry() {
+   let form1 = document.getElementById('form1');
+   form1.classList.toggle("be-visible");
+   let form2 = document.getElementById('form2');
+   form2.classList.toggle("be-visible");
+   console.log("clicked");
 }
